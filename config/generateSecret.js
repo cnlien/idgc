68ba7602ad192156b81cddd59ba75fc5022850b4
Chris Lien <clien@sharpencx.com>
2023-09-22 15:50:08 -0400

    Fixed prettier and linting issues.

--- config/generateSecret.js
+++ config/generateSecret.js
@@ -1,5 +1,5 @@
 module.exports = function generateSecret() {
-  const crypto = require('crypto');
-  const secret = crypto.randomBytes(64).toString('hex');
-  return secret;
-};
+  const crypto = require('crypto')
+  const secret = crypto.randomBytes(64).toString('hex')
+  return secret
+}
--- index.js
+++ index.js
@@ -1,44 +1,52 @@
-require('dotenv').config();
-const db = require('./config/database');  // Import the database connection
-const express = require('express');
-const session = require('express-session');
-const passport = require('passport');
-const app = express();
-const port = process.env.API_PORT;
+require('dotenv').config()
+const db = require('./config/database') // Import the database connection
+const express = require('express')
+const session = require('express-session')
+const passport = require('passport')
+const app = express()
+const port = process.env.API_PORT
 
 // Passport configuration
-require('./config/passport');
+require('./config/passport')
 
 // Express session setup
-app.use(session({ 
+app.use(
+  session({
     secret: require('./config/generateSecret')(), // Use the generateSecret function
-    resave: false, 
-    saveUninitialized: true 
-}));
+    resave: false,
+    saveUninitialized: true,
+  })
+)
 
 // Passport initialization
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
-app.use(express.json());
+app.use(express.json())
 
 // Import and use the routers
 
 // Google authentication route
-app.get('/auth/google', passport.authenticate('google', {
-  scope: ['profile', 'email']
-}));
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  })
+)
 
 // Callback route after Google authentication
-app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
-  res.redirect('/'); // Redirect to the homepage or dashboard after successful login
-});
-
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('/') // Redirect to the homepage or dashboard after successful login
+  }
+)
 
 app.get('/', (req, res) => {
-  res.send('<h1>Indy Disc Golf API</h1><p>Author: Chris Lien</p>');
-});
+  res.send('<h1>Indy Disc Golf API</h1><p>Author: Chris Lien</p>')
+})
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+  console.log(`Server is running on http://localhost:${port}`)
+})
